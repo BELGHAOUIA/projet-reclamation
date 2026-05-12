591551e7ce9251604c0b7bd91d160607da484988
BELGHAOUIA <belghaouiaamal@gmail.com>
2026-05-12 16:14:34 +0100

    updated config

--- ticket-service/src/main/java/com/reclamation/ticket_service/ConfigParams.java
+++
@@ -1,7 +0,0 @@
-package com.reclamation.ticket_service;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "global.params")
-public record ConfigParams(int p1, int p2) {
-}
