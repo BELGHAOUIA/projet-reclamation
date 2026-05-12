2d1b4d0f3b3fda1b5fd33ef6e0c2e146034d616a
Bouthaina19 <boutheinabelg1@gmail.com>
2026-05-12 12:21:20 +0100

    test

--- ticket-service/src/main/java/com/reclamation/ticket_service/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/reclamation/ticket_service/TicketServiceApplication.java
@@ -2,8 +2,10 @@ package com.reclamation.ticket_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ConfigParams.class)
 public class TicketServiceApplication {
 
 	public static void main(String[] args) {
